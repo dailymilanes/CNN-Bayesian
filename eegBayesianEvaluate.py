d75652de025363b31ca74ad7817c59db11b5ac16
Daily Milanes Hermosilla <65421251+dailymilanes@users.noreply.github.com>
2022-10-30 08:12:30 -0500

    Create eegBayesianEvaluate.py

---
+++ eegBayesianEvaluate.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow_probability as tfp
+import pandas as pd
+import seaborn as sns
+import time
+from sklearn.model_selection import train_test_split
+import math
+import os
+from tensorflow.keras import backend as K
+import tensorflow.keras.utils
+import scipy.io as sio
+import random as random
+from tensorflow.keras.layers import Conv2D, Input, Flatten, Dense, BatchNormalization, Dropout, SeparableConv2D
+from tensorflow.keras.constraints import max_norm
+from tensorflow.keras.models import Model
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+from scipy import stats as st
+import scipy.io as sio
+from sklearn.model_selection import StratifiedKFold
+import types
+# Dependency imports
+import numpy as np
+import tensorflow.compat.v1 as tf1
+import tensorflow.compat.v2 as tf
+from tensorflow_probability.python import util as tfp_util
+from tensorflow_probability.python.distributions import deterministic as deterministic_lib
+from tensorflow_probability.python.distributions import independent as independent_lib
+from tensorflow_probability.python.distributions import normal as normal_lib
+import eegBayesianUtils
+import modelBayesian
+
+def Evaluate(subject, cropDistance, cropSize, model):
+         
+    if  subject[0] == 'A':
+        nb_classes=4
+        channels=22
+        dropoutRate=0.8
+        folds=9
+        dataDirectory = '../../DataBase/'  
+        weightsDirectory=dataDirectory +'SubjectSpecific/New_model/Bayesian/Con_prior/Raiz/'
+    else:
+        nb_classes=2
+        channels=3
+        dropoutRate=0.5
+        folds=10
+        dataDirectory='../../DataBase/Data2b/'
+        weightsDirectory=dataDirectory + 'SubjectSpecific/New_model/Bayesian/'  
+    droputStr = "%0.2f" % dropoutRate 
+        
+    datalist, labelslist = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', ['Left','Right','Foot','Tongue'])
+    tensor_mc_after=np.zeros((50,len(datalist),int(math.ceil((1125-cropSize)/cropDistance)),nb_classes))
+    for i in range(1,17):  
+      cv = StratifiedKFold(n_splits = folds, random_state=i)
+      pseudoTrialList = range(len(datalist))
+      pseudolabelList = np.array(labelslist)
+              
+      for train_indices, test_indices in cv.split(pseudoTrialList, pseudolabelList): 
+         test_data, test_labels = eeg.Bayesian.Utils.makeNumpys1(datalist, labelslist, cropDistance, cropSize, nb_classes, test_indices)
+         if model==MCD:
+            classifier=SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
+            classifier.load_weights(weightsDirectory + subject+'_SE_Bayesian'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
+            prediction_mc_after =[classifier.predict(test_data, batch_size=32) for _ in range(1)]
+         elif model==moped:
+            classifier=SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
+            classifier.load_weights(weightsDirectory + subject+'_SE_Bayesian'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
+            prediction_mc_after =[classifier.predict(test_data, batch_size=32) for _ in range(50)]
+         else:
+            classifier=SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
+            classifier.load_weights(weightsDirectory + subject+'_SE_Bayesian'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
+            prediction_mc_after =[classifier.predict(test_data, batch_size=32) for _ in range(50)]
+            
+         prediction_mc_after = np.array(prediction_mc_after)
+         prediction_mc_after=prediction_mc_after.reshape((50,len(test_indices),int(math.ceil((1125-cropSize)/cropDistance)),nb_classes))
+
+         for k in range(len(test_indices)):
+           j=test_indices[k]
+           tensor_mc_after[:,j,:,:]=prediction_mc_after[:,k,:,:]
+
+      np.save(weightsDirectory+'tensor_SE_Bayesian'+subject+model+'_seed_'+str(i)+'_sin_drop_after.npy', tensor_mc_after, allow_pickle=False)  
+        
+        
+        
