d38883d79cd4b9cab5e12755ac112c288f91199e
Daily Milanes Hermosilla <65421251+dailymilanes@users.noreply.github.com>
2022-10-30 08:27:27 -0500

    Update eegBayesianEvaluate.py

--- eegBayesianEvaluate.py
+++ eegBayesianEvaluate.py
@@ -33,22 +33,20 @@ from tensorflow_probability.python.distributions import normal as normal_lib
 import eegBayesianUtils
 import modelBayesian
 
-def Evaluate(subject, cropDistance, cropSize, model):
+def Evaluate(subject, cropDistance, cropSize, model, datadirectory, weightsDirectory):
          
     if  subject[0] == 'A':
         nb_classes=4
         channels=22
         dropoutRate=0.8
         folds=9
-        dataDirectory = '../../DataBase/'  
-        weightsDirectory=dataDirectory +'SubjectSpecific/New_model/Bayesian/Con_prior/Raiz/'
+        
     else:
         nb_classes=2
         channels=3
         dropoutRate=0.5
         folds=10
-        dataDirectory='../../DataBase/Data2b/'
-        weightsDirectory=dataDirectory + 'SubjectSpecific/New_model/Bayesian/'  
+        
     droputStr = "%0.2f" % dropoutRate 
         
     datalist, labelslist = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', ['Left','Right','Foot','Tongue'])
@@ -61,15 +59,15 @@ def Evaluate(subject, cropDistance, cropSize, model):
       for train_indices, test_indices in cv.split(pseudoTrialList, pseudolabelList): 
          test_data, test_labels = eeg.Bayesian.Utils.makeNumpys1(datalist, labelslist, cropDistance, cropSize, nb_classes, test_indices)
          if model==MCD:
-            classifier=SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
-            classifier.load_weights(weightsDirectory + subject+'_SE_Bayesian'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
-            prediction_mc_after =[classifier.predict(test_data, batch_size=32) for _ in range(1)]
+            classifier=modelBayesian.createModel(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
+            classifier.load_weights(weightsDirectory + subject+'_SE_'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
+            prediction_mc_after =[classifier(test_data, training=True) for _ in range(50)]
          elif model==moped:
-            classifier=SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
+            classifier=modelBayesian.SCNBayesianTL(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
             classifier.load_weights(weightsDirectory + subject+'_SE_Bayesian'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
             prediction_mc_after =[classifier.predict(test_data, batch_size=32) for _ in range(50)]
          else:
-            classifier=SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
+            classifier=modelBayesian.SCNBayesian(nb_classes = nb_classes,Chans = channels,Samples = cropSize,dropoutRate=dropoutRate, cropDistance=cropDistance)  
             classifier.load_weights(weightsDirectory + subject+'_SE_Bayesian'+model+'_d_'+droputStr +'_c_'+str(2)+'_seed' +str(i)+'_exp_'+ str(i)+'_no_drop_weights.hdf5')
             prediction_mc_after =[classifier.predict(test_data, batch_size=32) for _ in range(50)]
             
