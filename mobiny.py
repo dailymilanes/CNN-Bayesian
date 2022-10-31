26a0c6cc541582860bdae4d9dfa23054c0865e9f
Daily Milanes Hermosilla <65421251+dailymilanes@users.noreply.github.com>
2022-10-31 08:05:57 -0500

    Update mobiny.py

--- mobiny.py
+++ mobiny.py
@@ -9,29 +9,24 @@ import os
 from scipy import stats as st
 import scipy.io as sio
 from sklearn.model_selection import StratifiedKFold
-import eegBayesianUtils
-import modelBayesian
 import eegBayesianEvaluate
 
-def mobiny method():
+def mobiny method(subject, ):
     if  subject[0] == 'A':
         nb_classes=4
         channels=22
         dropoutRate=0.8
         folds=6       
-        dataDirectory = '../../DataBase/'  
-        weightsDirectory=dataDirectory +'SubjectSpecific/New_model/Bayesian/Con_prior/'
-        
+                
     else:
         nb_classes=2
         channels=3
         dropoutRate=0.5
         folds=5  
-        dataDirectory='../../DataBase/Data2b/'
-        weightsDirectory=dataDirectory + 'SubjectSpecific/New_model/Bayesian/Con_prior/'  
     droputStr = "%0.2f" % dropoutRate 
     
-    datalist, labelslist = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', ['Left','Right','Foot','Tongue'])
+    datalist, labelslist = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Training/', ['Left','Right','Foot','Tongue'])
+    datalist1, labelslist1 = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', ['Left','Right','Foot','Tongue'])
     threshold=np.arange(0.05,1.05,0.05)
     ind1=0
     ua=np.zeros([len(seeds),len(threshold)])
@@ -39,7 +34,6 @@ def mobiny method():
        cv = StratifiedKFold(n_splits = folds, random_state=n, shuffle=True)
        pseudoTrialList = range(len(datalist))
        pseudolabelList = np.array(labelslist)
-              
        for train_indices, test_indices in cv.split(pseudoTrialList, pseudolabelList): 
          break  
        labelslist=np.array(labelslist)
@@ -83,19 +77,21 @@ def mobiny method():
                    
          ua=(ciertos_good+inciertos_bad)/(ciertos_good+ciertos_bad+inciertos_good+inciertos_bad)    
          max=np.argmax(ua, axis=-1)
+
+            # Now, we determinate the UA from optimal threshold selected over validation set
          tensor_test=eegBayesianEvaluate.Validation(subject, cropDistance, cropSize, model, datadirectory, weightsDirectory)   
          mean=np.mean(tensor_test, axis=0)  
          y=np.argmax(mean, axis=-1)
          true=1*(y==label)
-         test=np.zeros((len(datalist),63))
+         test=np.zeros((len(datalist1),int(math.ceil((1125-cropSize)/cropDistance))))
          entropy=st.entropy(mean,base=2,axis=-1)/np.log2(nb_classes)
          ciertos_good=0
          ciertos_bad=0
          inciertos_good=0
          inciertos_bad=0
-         for i in range(len(datalist)):
+         for i in range(len(datalist1)):
             for j in range(int(math.ceil((1125-cropSize)/cropDistance))): 
-                if entropy[i,j] < m[a]/20+0.05:
+                if entropy[i,j] < m/len(threshold)+0.05:
                      test[i,j]=True
                      if true[i,j]==True:
                          ciertos_good=ciertos_good + 1
@@ -108,7 +104,9 @@ def mobiny method():
                          inciertos_good=inciertos_good + 1
                        else:
                          inciertos_bad=inciertos_bad+1
-                   
+       
+    
+    
       moviny_result[a,0]=ciertos_good
       moviny_result[a,1]=inciertos_good
       moviny_result[a,2]=ciertos_bad
