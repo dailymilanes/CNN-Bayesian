37f45c9607346a00c81ba92f1ad75a35b0a216dd
Daily Milanes Hermosilla <65421251+dailymilanes@users.noreply.github.com>
2022-11-01 09:34:47 -0500

    Update Adaptative_threshold.py

--- Adaptative_threshold.py
+++ Adaptative_threshold.py
@@ -10,8 +10,9 @@ from scipy import stats as st
 import scipy.io as sio
 from sklearn.model_selection import StratifiedKFold
 import eegBayesianEvaluate
+import eegBayesianUtils
 
-def adaptative_threshold(subject, ):
+def adaptative_threshold(subject, model, ):
     if  subject[0] == 'A':
         nb_classes=4
         channels=22
@@ -26,10 +27,7 @@ def adaptative_threshold(subject, ):
     droputStr = "%0.2f" % dropoutRate 
     
     datalist1, labelslist1 = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', ['Left','Right','Foot','Tongue'])
-    ind1=0
-    ind2=0
-    ua_mobiny=np.zeros(len(threshold))
-    mobiny_result=np.zeros([16,4])   # 16 amount of seeds, and 4 groups, cc, ci, ic, ii
+    ind=0
     for n in range(1,17):   
        tensor_test=eegBayesianEvaluate.Evaluation(subject, cropDistance, cropSize, model, datalist1, weightsDirectory, seed=n) 
        labelslist1=np.array(labelslist1)
@@ -45,30 +43,28 @@ def adaptative_threshold(subject, ):
        inciertos_bad=0
        dif=np.zeros(50) 
        a=np.zeros(50)
-       desv=np.zeros((len(datalist),63))
-       mean_dif=np.zeros((len(datalist),63))
-       ztest_99=np.zeros((len(datalist),63))
-       ztest_95=np.zeros((len(datalist),63))
-       value_ztest_95=np.zeros((len(datalist),63))
-       value_ztest_99=np.zeros((len(datalist),63))
+       desv=np.zeros((len(datalist),int(math.ceil((1125-cropSize)/cropDistance))))
+       mean_dif=np.zeros((len(datalist),int(math.ceil((1125-cropSize)/cropDistance))))
+       ztest_99=np.zeros((len(datalist),int(math.ceil((1125-cropSize)/cropDistance))))
+       ztest_95=np.zeros((len(datalist),int(math.ceil((1125-cropSize)/cropDistance))))
+       value_ztest_95=np.zeros((len(datalist),int(math.ceil((1125-cropSize)/cropDistance))))
+       value_ztest_99=np.zeros((len(datalist),int(math.ceil((1125-cropSize)/cropDistance))))
        for i in range(len(datalist)):
             for j in range(int(math.ceil((1125-cropSize)/cropDistance))):
-                mean_max=np.argmax(mean[i,j,:])       #indice del maximo de las medias x clase
-                mean_entropy=st.entropy(mean[i,j,:],base=2,axis=-1)  # la entropia de la media
+                mean_max=np.argmax(mean[i,j,:])       #maximum index from the average of 50 forward pass per class
                 for k in range(50):
                     a[k]=np.argmax(tensor_test[k,i,j])
-                    indexs=np.argsort(-1*tensor_test[k,i,j,:])    #indices de las salidas ordenadas de mayor a menor valor
+                    indexs=np.argsort(-1*tensor_test[k,i,j,:])    #index of the outputs sorted from highest to lowest value
                     if indexs[0]==mean_max:
                         dif[k]=tensor_test[k,i,j,mean_max]-tensor_test[k,i,j,indexs[1]]
                     else:
                         dif[k]=tensor_test[k,i,j,mean_max]-tensor_test[k,i,j,indexs[0]]
-                         
-            
+                                     
                 desv[i,j]=np.std(dif)
                 mean_dif[i,j]=np.mean(dif)
                 
-           # New_test with 99% of confidence level (2.326)
-                if mean_dif[i,j]>desv[i,j]*2.326/math.sqrt(50):
+           # New_test with 99% of confidence level (2.326) using margen of confidence as metric of uncertainty
+                if mean_dif[i,j]> desv[i,j]*2.326/math.sqrt(50):
                    ztest_99[i,j]=True
                    value_ztest_99[i,j]=mean_dif[i,j]-desv[i,j]*2.326/math.sqrt(50)
                
@@ -76,8 +72,8 @@ def adaptative_threshold(subject, ):
                    ztest_99[i,j]=False
                    value_ztest_99[i,j]=mean_dif[i,j]-desv[i,j]*2.326/math.sqrt(50)
                
-           # New_test with 95% of confidence level (1.645)
-                if mean_dif[i,j]>desv[i,j]*1.645/math.sqrt(50):
+           # New_test with 95% of confidence level (1.645) using margen of confidence as metric of uncertainty
+                if mean_dif[i,j]> desv[i,j]*1.645/math.sqrt(50):
                    ztest_95[i,j]=True
                    value_ztest_95[i,j]=mean_dif[i,j]-desv[i,j]*1.645/math.sqrt(50)
                 else:
@@ -114,19 +110,19 @@ def adaptative_threshold(subject, ):
                 else:
                     inciertos_bad_99 = inciertos_bad_99 + 1     
         
-        resultados_95[ind,0]=ciertos_good_95
-        resultados_95[ind,1]=inciertos_good_95
-        resultados_95[ind,2]=ciertos_bad_95
-        resultados_95[ind,3]=inciertos_bad_95
+       resultados_95[ind,0]=ciertos_good_95
+       resultados_95[ind,1]=inciertos_good_95
+       resultados_95[ind,2]=ciertos_bad_95
+       resultados_95[ind,3]=inciertos_bad_95
    
-        resultados_99[ind,0]=ciertos_good_99
-        resultados_99[ind,1]=inciertos_good_99
-        resultados_99[ind,2]=ciertos_bad_99
-        resultados_99[ind,3]=inciertos_bad_99
-        ind=ind+1
-     
-   ua_result=(np.sum(mobiny_result[:,0])+np.sum(mobiny_result[:,3]))/(np.sum(mobiny_result[:,0])+np.sum(mobiny_result[:,1])+np.sum(mobiny_result[:,2])+np.sum(mobiny_result[:,3]))*100   
-   rcc
-   rcu
-   rc
+       resultados_99[ind,0]=ciertos_good_99
+       resultados_99[ind,1]=inciertos_good_99
+       resultados_99[ind,2]=ciertos_bad_99
+       resultados_99[ind,3]=inciertos_bad_99
+       ind=ind+1
+   rc=(np.sum(resultados_95[:,0])+np.sum(resultados_95[:,2]))/(np.sum(resultados_95[:,0])+np.sum(resultados_95[:,1])+np.sum(resultados_95[:,2])+np.sum(resultados_95[:,3]))*100  
+   rcc=np.sum(resultados_95[:,0])/(np.sum(resultados_95[:,0])+np.sum(resultados_95[:,2]))*100  
+   rcu 
+   ua=(np.sum(resultados_95[:,0])+np.sum(resultados_95[:,3]))/(np.sum(resultados_95[:,0])+np.sum(resultados_95[:,1])+np.sum(resultados_95[:,2])+np.sum(resultados_95[:,3]))*100   
+   
    return ua_result           
