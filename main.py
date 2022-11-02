782b86f5f9e519a375564d07fc14c7c2af315b95
Daily Milanes Hermosilla <65421251+dailymilanes@users.noreply.github.com>
2022-11-01 20:40:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,29 +10,22 @@ Created on Thu Feb 11 20:09:07 2021
 
 # To ensure repeatability of the experiment #2, #3 and #4, please use seed=1 up to 16
 
-# To run any experiment, select appropietly subject, seed, dropoutRate, cropDistance=2
-# cropSize=1000 to datasets 2a and 2b, cropSize=750 to datset IVa
+# To run any experiment, select appropietly subject, seed (1 to 16), dropoutRate, cropDistance=2, cropSize=1000 
 
-# nb_classes=4, channels=22, fraction=6 to dataset 2a
-# nb_classes=2, channels=3, fraction=5 to dataset 2b
-# nb_classes=2, channels=118 to dataset IVa
-
-# Tu run experiment #4, please exclude parameter must be different of 0, and specify unknown subject
-
-
+# dropoutRate=0.8, nb_classes=4, channels=22, fraction=6 to dataset 2a
+# dropoutRate=0.5, nb_classes=2, channels=3, fraction=5 to dataset 2b
 
 import eegBayesianTrain
 import eegBayesianEvaluate
 
 global dataDirectory
-eegBayesianTrain.dataDirectory = '../Data/'    
+eegBayesianTrain.dataDirectory = '../Data/'     # Place where there are the datasets
 
 global weightsDirectory 
-eegBayesianTrain.weightsDirectory = '../Weights/'
+eegBayesianTrain.weightsDirectory = '../Weights/'   # Place where the weights will be saved
 
 
 ## To train Bayesian Neural Network models using subject-specific (SE) strategy, specify subject, dropout level, distance between crops, size of crops, model 'MOPED' or 'NORMAL'
-
 # eegBayesianTrain.intraSubjectTrain('B01', dropoutRate=0.5, cropDistance = 2, cropSize = 1000, model='MOPED')    # for example to train subject B01 of dataset 2b with Moped method
 
 ## To train Bayesian Neural Network models using non-subject-specific (NSE) strategy
