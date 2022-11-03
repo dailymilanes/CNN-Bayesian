0dd22501369167a0d52741768bfd0f279a77d799
Daily Milanes Hermosilla <65421251+dailymilanes@users.noreply.github.com>
2022-11-03 10:56:12 -0500

    Update mobiny.py

--- mobiny.py
+++ mobiny.py
@@ -29,8 +29,8 @@ def mobiny_method(subject, cropDistance=2, cropSize=1000, model='MCD', type_trai
         dropoutRate=0.5
     droputStr = "%0.2f" % dropoutRate 
     
-    datalist, labelslist = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Training/', ['Left','Right','Foot','Tongue'])
-    datalist1, labelslist1 = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', ['Left','Right','Foot','Tongue'])
+    datalist, labelslist = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Training/', strLabels)
+    datalist1, labelslist1 = eegBayesianUtils.load_eeg(dataDirectory + subject+'/Evaluating/', strLabels)
     threshold=np.arange(0.05,1.05,0.05)
     ind1=0
     ind2=0
@@ -46,48 +46,40 @@ def mobiny_method(subject, cropDistance=2, cropSize=1000, model='MCD', type_trai
        text_label= labelslist[test_indices]
        label=np.repeat(text_label,int(math.ceil((1125-cropSize)/cropDistance)))
        label=label.reshape(len(text_label),int(math.ceil((1125-cropSize)/cropDistance)))   
-       tensor_val=eegBayesianEvaluate.Validation(subject, cropDistance=2, cropSize=1000, model='MCD', accuracy=True, type_training='SE')
+       tensor_val=eegBayesianEvaluate.Validation(subject, datalist, labelslist, nb_classes, folds=5, cropDistance=2, cropSize=1000, seed=n, model='MCD', type_training='SE')
        mean=np.mean(tensor_val, axis=0)  
        y=np.argmax(mean, axis=-1)
        true=1*(y==label)    
        list_true=true.reshape(len(test_indices)*int(math.ceil((1125-cropSize)/cropDistance)))
         
-       goods_list=np.where(list_true==True)
-       bads_list=np.where(list_true==False)
-       goods=np.sum(list_true==True)
-       bads=np.sum(list_true==False)
-        
-       dif=np.zeros(50) 
-       desv=np.zeros((len(test_indices),int(math.ceil((1125-cropSize)/cropDistance))))
-       mean_dif=np.zeros((len(test_indices),int(math.ceil((1125-cropSize)/cropDistance))))
        test=np.zeros((len(test_indices),int(math.ceil((1125-cropSize)/cropDistance))))
        entropy=st.entropy(mean,base=2,axis=-1)/np.log2(nb_classes)
        for m in threshold:
-          ciertos_good=0
-          ciertos_bad=0
-          inciertos_good=0
-          inciertos_bad=0
+          certain_good=0
+          certain_bad=0
+          uncertain_good=0
+          uncertain_bad=0
           for i in range(len(test_indices)):
             for j in range(int(math.ceil((1125-cropSize)/cropDistance))): 
                 if entropy[i,j]<m:
                      test[i,j]=True
                      if true[i,j]==True:
-                         ciertos_good=ciertos_good + 1
+                         certain_good=certain_good + 1
                      else:
-                         ciertos_bad=ciertos_bad+1
+                         certain_bad=certain_bad+1
                 else:
                        test[i,j]=False
                        if true[i,j]==True:
-                         inciertos_good=inciertos_good + 1
+                         uncertain_good=uncertain_good + 1
                        else:
-                         inciertos_bad=inciertos_bad+1
+                         uncertain_bad=uncertain_bad+1
                    
-          ua_mobiny[ind1]=(ciertos_good+inciertos_bad)/(ciertos_good+ciertos_bad+inciertos_good+inciertos_bad)*100
+          ua_mobiny[ind1]=(certain_good+uncertain_bad)/(certain_good+certain_bad+uncertain_good+uncertain_bad)*100
           ind1=ind1+1
        max=np.argmax(ua_mobiny, axis=-1)
 
             # Now, we determinate the UA from optimal threshold selected over validation set
-       tensor_test=eegBayesianEvaluate.Evaluate(subject, cropDistance, cropSize, model, datadirectory, weightsDirectory, seed=n) 
+       tensor_test=eegBayesianEvaluate.evaluation(subject, datalist1, labelslist1, nb_classes, folds=5, cropDistance=2, cropSize=1000, seed=n, model='MCD', type_training='SE') 
        labelslist1=np.array(labelslist1)
        label=np.repeat(labelslist1,int(math.ceil((1125-cropSize)/cropDistance)))
        label=label.reshape(len(datalist1),int(math.ceil((1125-cropSize)/cropDistance)))   # label is a matrix of len(datalist1)x63
@@ -95,30 +87,30 @@ def mobiny_method(subject, cropDistance=2, cropSize=1000, model='MCD', type_trai
        y=np.argmax(mean, axis=-1)
        true=1*(y==label)
        test=np.zeros((len(datalist1),int(math.ceil((1125-cropSize)/cropDistance))))
-       entropy=st.entropy(mean,base=2,axis=-1)/np.log2(nb_classes)
-       ciertos_good=0
-       ciertos_bad=0
-       inciertos_good=0
-       inciertos_bad=0
+       entropy_test=st.entropy(mean,base=2,axis=-1)/np.log2(nb_classes)
+       ciertos_good_test=0
+       ciertos_bad_test=0
+       inciertos_good_test=0
+       inciertos_bad_test=0
        for i in range(len(datalist1)):
           for j in range(int(math.ceil((1125-cropSize)/cropDistance))): 
-              if entropy[i,j] < max/len(threshold)+0.05:
+              if entropy_test[i,j] < max/len(threshold)+0.05:
                  test[i,j]=True
                  if true[i,j]==True:
-                     ciertos_good=ciertos_good + 1
+                     ciertos_good_test=ciertos_good_test + 1
                  else:
-                      ciertos_bad=ciertos_bad+1
+                      ciertos_bad_test=ciertos_bad_test+1
               else:
                  test[i,j]=False
                  if true[i,j]==True:
-                     inciertos_good=inciertos_good + 1
+                     inciertos_good_test=inciertos_good_test + 1
                  else:
-                     inciertos_bad=inciertos_bad+1
+                     inciertos_bad_test=inciertos_bad_test+1
        
-       mobiny_result[ind2,0]=ciertos_good
-       mobiny_result[ind2,1]=inciertos_good
-       mobiny_result[ind2,2]=ciertos_bad
-       mobiny_result[ind2,3]=inciertos_bad
+       mobiny_result[ind2,0]=ciertos_good_test
+       mobiny_result[ind2,1]=inciertos_good_test
+       mobiny_result[ind2,2]=ciertos_bad_test
+       mobiny_result[ind2,3]=inciertos_bad_test
        ind2==ind2+1    
     ua_result=(np.sum(mobiny_result[:,0])+np.sum(mobiny_result[:,3]))/(np.sum(mobiny_result[:,0])+np.sum(mobiny_result[:,1])+np.sum(mobiny_result[:,2])+np.sum(mobiny_result[:,3]))*100   
     return ua_result           
